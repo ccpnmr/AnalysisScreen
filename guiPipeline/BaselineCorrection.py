5c93edbe8ed15e857f18d48cf18a42e1d48099b3
Luca Mureddu <lgm8@leicester.ac.uk>
2017-05-23 16:26:31 +0100

    updates

--- guiPipeline/BaselineCorrection.py
+++ guiPipeline/BaselineCorrection.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtGui
 from ccpn.ui.gui.widgets.Frame import Frame
 from ccpn.ui.gui.widgets.Label import Label
-from ccpn.ui.gui.widgets.PulldownList import PulldownList
+from ccpn.ui.gui.widgets.CheckBox import CheckBox
 from ccpn.ui.gui.widgets.PipelineWidgets import PipelineBox, PipelineDropArea
 from collections import OrderedDict
 
@@ -42,31 +42,36 @@ class BaselineCorrection(PipelineBox):
   def applicationsSpecific(self):
     return ['AnalysisScreen']
 
-  def _baselineCorrection(self, spectra):
+  @property
+  def _inputData(self):
+    if self.parent is not None:
+      return list(self.parent._inputData)
+    else:
+      return []
+
+  def _baselineCorrection(self, spectra, flatten=False, ratio=2):
     correctedSpectra = []
     for sp in spectra:
       from ccpn.AnalysisScreen.lib.spectralProcessing.baselineCorrection import CWBC
       x = sp.positions
       y = sp.intensities
-      newX, newy = CWBC.baselineCorrection(x, y, flatten=True)
+      newX, newy = CWBC.baselineCorrection(x, y, flatten=flatten, ratio=ratio )
       sp._positions = newX
       sp._intensities = newy
       correctedSpectra.append(sp)
     return correctedSpectra
 
   def runMethod(self):
+    removeNoise = self.getWidgetsParams()['flattenCheckBox']
 
-    print('Running ',  self.name())
-    print(self._getInputData())
-    # if self.project is not None:
-    #   correctedSpectra = self._baselineCorrection(self.project.spectra)
-    print('Done ', self.name())
+    if self.project is not None:
+      if len(self._inputData)>0:
+        print('Running ',  self.name() ,' on ', self._inputData)
+        correctedSpectra = self._baselineCorrection(self._inputData, flatten=removeNoise)
+        print('Done ', self.name())
+      print('No data. Drag and drop SP or SG on the settings inputData box')
 
-  def _getInputData(self):
-    if self.parent is not None:
-      return list(self.parent._inputData)
-    else:
-      return []
+
 
   def _setMainLayout(self):
     self.mainFrame = Frame(None, setLayout=False)
@@ -76,18 +81,21 @@ class BaselineCorrection(PipelineBox):
 
   def _createWidgets(self):
     self.spectrumLabel = Label(self, 'Baseline Correction',)
-
+    self.flattenCheckBox = CheckBox(self, text='Remove Noise')
 
     self.mainLayout.addWidget(self.spectrumLabel, 0, 0)
+    self.mainLayout.addWidget(self.flattenCheckBox, 0, 1)
+
 
   def getWidgetsParams(self):
 
-    # spectrumPulldown = self.spectrumPulldown.currentText()
-    #
-    # params = OrderedDict([
-    #                       ('spectrumPulldown', spectrumPulldown),
-    #                       ])
-    # self.params = params
+    flattenCheckBox = self.flattenCheckBox.get()
+
+    params = OrderedDict([
+                          ('flattenCheckBox', flattenCheckBox),
+                          ])
+    self.params = params
+    print(self.params)
     return self.params
 
 
